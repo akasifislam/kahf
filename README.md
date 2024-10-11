5179359cd7a50b77bbee37660a94f50430058307
akasifislam <aseasifislam@gmail.com>
2024-10-12 01:33:37 +0600

    seeder

--- README.md
+++ README.md
@@ -23,3 +23,68 @@ The Laravel framework is open-sourced software licensed under the [MIT license](
     -   Assume that every user will take their vaccine on their scheduled date. Show the Vaccinated status if the scheduled date is passed.
 
 -   php artisan vaccine:send-reminders
+
+# Using
+
+-   Laravel-10
+-   Breeze
+-   PHP 8.1
+-   Mail Gmail Configration
+
+### Downlaod and Clone
+
+    -   git clone -b dev https://github.com/akasifislam/kahf
+
+# Larave10:
+
+    -   composer install
+    -   npm install && npm run dev
+    -   cp .env.example .env
+    -   php artisan key:generate
+    -   php artisan migare --seed
+    -   php artisan serve --port=9000
+    -   php artisan migrate:fresh --seed
+    ## login credential
+
+    - email: admin@mail.com
+    - password: secret007
+
+### Home Page
+
+![hh](https://github.com/user-attachments/assets/4f928bf1-38d6-425f-afd4-062e216b368a)
+
+### Vaccine Center
+
+![11](https://github.com/user-attachments/assets/da47c007-ce17-46e3-a3cd-79535db7130d)
+
+### Vaccine Schedule
+
+![2](https://github.com/user-attachments/assets/dba9d9cd-ba8f-42d4-9937-feb0b054e65e)
+
+### Registration and Search
+
+![3](https://github.com/user-attachments/assets/4c68b964-67e9-4bce-b3a5-8c0b8e15ef65)
+
+### Registration form
+
+![4](https://github.com/user-attachments/assets/21d0b9a4-a7b8-4821-a57b-36b0374029e8)
+
+### Schedule Card
+
+![5](https://github.com/user-attachments/assets/0c760114-d1a5-4dd4-b929-9796fa496b68)
+
+### Not Register
+
+![6](https://github.com/user-attachments/assets/bca51d25-ff16-4cbe-b558-ae4a284653c4)
+
+### Vaccinated
+
+![7](https://github.com/user-attachments/assets/bc1ca8bc-f79f-4353-8b24-780d5cd7d744)
+
+###
+
+![11](https://github.com/user-attachments/assets/d3f29b95-5ba4-4d9c-bada-8d6096c4a65d)
+
+### Mail Configration
+
+![mail](https://github.com/user-attachments/assets/64547ebc-b2dd-4eaf-93ad-bd74e9c2c732)
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Admin\VaccineCenter;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +22,61 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        User::create([
+            'name' => "Admin",
+            'email' => "admin@mail.com",
+            'password' => Hash::make('secret007'),
+        ]);
+
+        // Create 8 vaccine centers with Bangladeshi names and addresses
+        $vaccineCenters = [
+            [
+                'center_name' => 'Dhaka Medical College Hospital',
+                'address' => 'Shaheed Minar Road, Dhaka-1000',
+                'daily_limit' => 100,
+            ],
+            [
+                'center_name' => 'Chittagong Medical College Hospital',
+                'address' => 'Gulzar Circle, Chattogram-4000',
+                'daily_limit' => 75,
+            ],
+            [
+                'center_name' => 'Sir Salimullah Medical College Hospital',
+                'address' => 'Mitford Road, Dhaka-1100',
+                'daily_limit' => 80,
+            ],
+            [
+                'center_name' => 'Rajshahi Medical College Hospital',
+                'address' => 'Luxmipur, Rajshahi-6000',
+                'daily_limit' => 90,
+            ],
+            [
+                'center_name' => 'Sylhet MAG Osmani Medical College Hospital',
+                'address' => 'Naiorpool, Sylhet-3100',
+                'daily_limit' => 60,
+            ],
+            [
+                'center_name' => 'Mymensingh Medical College Hospital',
+                'address' => 'Charpara, Mymensingh-2200',
+                'daily_limit' => 85,
+            ],
+            [
+                'center_name' => 'Khulna Medical College Hospital',
+                'address' => 'Boyra Main Road, Khulna-9000',
+                'daily_limit' => 50,
+            ],
+            [
+                'center_name' => 'Barisal Sher-e-Bangla Medical College Hospital',
+                'address' => 'Band Road, Barisal-8200',
+                'daily_limit' => 120,
+            ],
+        ];
+
+        // Insert vaccine centers into the database
+        foreach ($vaccineCenters as $center) {
+            VaccineCenter::create($center);
+        }
     }
 }
