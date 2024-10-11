f79f012b2f435f63c19a54c32cd33881d66cf1f2
akasifislam <aseasifislam@gmail.com>
2024-10-12 00:33:09 +0600

    vaccine mail send

--- resources/views/emails/vaccine-reminder.blade.php
+++ resources/views/emails/vaccine-reminder.blade.php
@@ -1,10 +1,25 @@
-<h1>Reminder: Your COVID-19 Vaccination is Tomorrow</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<p>Dear {{ $patient->name }},</p>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Your COVID-19</title>
+</head>
 
-<p>This is a reminder that your COVID-19 vaccination is scheduled for tomorrow at
-    {{ $patient->registration->scheduled_date }}.</p>
+<body>
+    <h1>Reminder: Your COVID-19 Vaccination is Tomorrow</h1>
 
-<p>Please remember to bring your ID and any required documents.</p>
+    <p>Dear {{ $patient->name }},</p>
 
-<p>Thank you for your participation in the vaccination program.</p>
+    <p>This is a reminder that your COVID-19 vaccination is scheduled for tomorrow at
+        {{ $patient->registration->scheduled_date }}.</p>
+
+    <p>Please remember to bring your ID and any required documents.</p>
+
+    <p>Thank you for your participation in the vaccination program.</p>
+
+</body>
+
+</html>
